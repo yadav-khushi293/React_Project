5d25fa7b2c9f140c8417b67aa98a151b2fdb8bbf
yadav-khushi293 <yadavkhushi293@gmail.com>
2026-05-06 16:08:56 +0530

    db json

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import { Navbar } from "./Pages/Navbar/navbar";
-import { HomePage } from "./Pages/HomePage/homePage";
-import { Men } from "./Pages/Men/men";
-import { Footware } from "./Pages/Men/Men_Footware/footware";
-import { Topware } from "./Pages/Men/Men_Topware/topware";
-import { ProdectPages } from "./ProductPage/ProductPages";
-import { Women_Footware } from "./Pages/Women_Footware/women_footware";
-import { Accessories } from "./ProductPage/Accessories";
-import { Ozark } from "./Pages/Ozark/ozark";
-import { Footer } from "./ProductPage/Footer";
+// import { Navbar } from "./Pages/Navbar/navbar";
+// import { HomePage } from "./Pages/HomePage/homePage";
+// import { Men } from "./Pages/Men/men";
+// import { Footware } from "./Pages/Men/Men_Footware/footware";
+// import { Topware } from "./Pages/Men/Men_Topware/topware";
+// import { ProdectPages } from "./ProductPage/ProductPages";
+// import { Women_Footware } from "./Pages/Women_Footware/women_footware";
+// import { Accessories } from "./ProductPage/Accessories";
+// import { Ozark } from "./Pages/Ozark/ozark";
+// import { Footer } from "./ProductPage/Footer";
+import {Cart} from "./Pages/Cartpage/Cart"
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
@@ -27,9 +28,11 @@ function App() {
         <Route path="/women_topware" element={<Women_Footware />}></Route>
         <Route path="/accessories" element={<Accessories />}></Route>
         <Route path="/ozark" element={<Ozark />}></Route>
-      </Routes>
+      </Routes> */}
 
       {/* <Footer /> */}
+
+      <Cart/>
     </>
   );
 }
---
+++ Frontend/src/Pages/Cartpage/Cart.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../Cartpage/Cart.css";
+
+export const Cart =()=>{
+
+
+    return(
+        <>
+        <div className="parents">
+          <div className="Description">Description</div>  
+
+          <div className="pragraph">Embrace sophistication with Red Tape Derby formal shoes for men in black, designed for both style and comfort. These formal shoes feature a premium leather upper that delivers a smooth, polished finish, ideal for office wear and formal occasions. The soft fabric lining offers a gentle touch, while the high-density foam insole ensures excellent cushioning and support for extended wear. The TPR sole enhances durability, flexibility, and reliable traction for confident steps. Featuring a classic low-cut Derby design, these shoes are finished with a round toe and flat heel for all-day comfort and style.</div>
+
+          <p className="Features">Features</p>
+
+         <div class="card">
+           <ul>
+      <li><strong>Leather Upper:</strong> Premium finish for an elegant formal appearance.</li>
+      <li><strong>Fabric Lining:</strong> Soft lining for comfortable wear all day.</li>
+      <li><strong>High Density Foam Insole:</strong> Provides superior cushioning and support.</li>
+      <li><strong>TPR Sole:</strong> Offers durability and flexibility with a reliable grip.</li>
+      <li><strong>Derby Design:</strong> Classic lace-up style for versatile formal occasions.</li>
+      <li><strong>Heel:</strong> Ensures stability and comfort for confident movements.</li>
+    </ul>
+  </div>
+  {/*  */}
+
+        <p className="Features">Product Specification</p>
+
+         <div class="card1">
+           <ul>
+      <li><strong>Material:</strong>  Sole – TPR | Upper – Leather | Lining – Fabric | Insole – High Density Foam</li>
+      <li><strong>Design:</strong> Low-cut Derby shoe style perfect for formal occasions</li>
+      <li><strong>Heel:</strong>Flat heel for balanced comfort and support</li>
+      <li><strong>Fit:</strong> Lace-up closure for a secure and adjustable fit</li>
+      <li><strong>Utility:</strong>Ideal for office wear, formal events, and business attire</li>
+      <li><strong>Country of Origin:</strong> India</li>
+      <li>Manufacturer/Packer/Importer Details: REDTAPE LIMITED, Plot No - 08, Sector 90, Noida, Gautam Buddha Nagar, Uttar Pradesh - 201301</li>
+    </ul>
+  </div>
+           
+        </div>
+        </>
+    )
+}
